bba6a08560bd567af8739f7decc7736648cbccd4
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-06-20 23:51:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './styles/App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <h1 className={styles.title}>WHERE'S WALDO?</h1>
+    </div>
+  );
+};
+
+export default App;
