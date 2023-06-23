682f30aa89f5664678a9036bf0ab0db621a24900
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-06-23 23:15:45 +0530

    refactor: change name of Puzzle component to PuzzleCard

--- src/components/PuzzleCard.jsx
+++ src/components/PuzzleCard.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
-import styles from '../styles/StartScreen.module.css';
+import styles from '../styles/PuzzleCard.module.css';
 
-export const Puzzle = ({ puzzle, setActivePuzzle }) => {
+export const PuzzleCard = ({ puzzle, setActivePuzzle }) => {
   const { image, location } = puzzle;
 
   return (
     <Link to="/game">
-      <div className={styles.puzzle} onClick={() => setActivePuzzle(puzzle)}>
+      <div
+        className={styles.puzzleCard}
+        onClick={() => setActivePuzzle(puzzle)}>
         <div className={styles.imageContainer}>
           <img src={image} alt={location} />
         </div>
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,4 +1,4 @@
-import { Puzzle } from './Puzzle';
+import { PuzzleCard } from './PuzzleCard';
 import { MessageScreen } from './MessageScreen';
 import { useDataFetch } from '../hooks/useDataFetch';
 import styles from '../styles/StartScreen.module.css';
@@ -20,9 +20,9 @@ export const StartScreen = ({ setActivePuzzle }) => {
       <h1 className={styles.title}>WHERE'S WALDO?</h1>
       <main className={styles.content}>
         <p>Search Waldo and his friends in...</p>
-        <div className={styles.puzzleContainer}>
+        <div className={styles.puzzleCards}>
           {puzzles.map((puzzle) => (
-            <Puzzle
+            <PuzzleCard
               key={puzzle.id}
               puzzle={puzzle}
               setActivePuzzle={setActivePuzzle}
