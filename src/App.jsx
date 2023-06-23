eb3b70a3ed6295c294a3f4385db0cd7abe501d69
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-06-23 18:59:27 +0530

    feat: add routing for navigation between screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import { StartScreen } from './components/StartScreen';
 import styles from './styles/App.module.css';
 
 const App = () => {
   return (
     <div className={styles.App}>
-      <StartScreen />
+      <Routes>
+        <Route path="/" element={<StartScreen />} />
+      </Routes>
     </div>
   );
 };
