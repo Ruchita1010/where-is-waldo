af354b77ed6ae54d401cd5d4f45284f956622015
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-06-23 20:08:22 +0530

    fix: puzzle image in GameScreen not showing up on refresh added local storage for persistent state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { StartScreen } from './components/StartScreen';
 import { GameScreen } from './components/GameScreen';
 import styles from './styles/App.module.css';
 
 const App = () => {
-  const [activePuzzle, setActivePuzzle] = useState({});
+  /* Reading from the local storage directly to set the initial state to ensure that we're getting the most recent value from the local storage, regardless of any async state updates */
+  const [activePuzzle, setActivePuzzle] = useState(() => {
+    return JSON.parse(localStorage.getItem('activePuzzle')) || {};
+  });
+
+  useEffect(() => {
+    localStorage.setItem('activePuzzle', JSON.stringify(activePuzzle));
+  }, [activePuzzle]);
 
   return (
     <div className={styles.App}>
